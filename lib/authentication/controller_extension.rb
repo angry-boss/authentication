31a790bf8663e8642fc4e50ba409c943abfb4be2
Administrator <ilmirashaikhraznova@gmail.com>
2020-04-30 19:21:31 +0200

    #2

--- lib/authentication/controller_extension.rb
+++ lib/authentication/controller_extension.rb
@@ -6,10 +6,8 @@ module Authentication
     def self.included(base)
       #base.helper_method :logged_in?, :current_user, :auth_session
       # !! helper_method for ActionController::API:Class
-      base.class_eval do
-        if respond_to?(:helper_method)
-          helper_method :logged_in?, :current_user, :auth_session
-        end
+      if respond_to?(:helper_method)
+        base.helper_method :logged_in?, :current_user, :auth_session
       end
       before_action_method = base.respond_to?(:before_action) ? :before_action : :before_filter
       base.public_send(before_action_method, :set_browser_id, :touch_auth_session)
